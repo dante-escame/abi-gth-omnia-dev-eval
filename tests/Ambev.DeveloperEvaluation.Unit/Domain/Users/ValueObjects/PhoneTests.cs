8e5c45bfb77a2a1b2600f67daba6a827ff7f51ec
dante-escame <danteescame@gmail.com>
2026-09-10 09:16:02 -0300

    tests: handling some tests adjustments

--- tests/Ambev.DeveloperEvaluation.Unit/Domain/Users/ValueObjects/PhoneTests.cs
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Users/ValueObjects/PhoneTests.cs
@@ -8,10 +8,10 @@ public class PhoneTests
 {
     [Theory(DisplayName = "Invalid phone is rejected on construction")]
     [InlineData("")]
-    [InlineData("11987654321")]
-    [InlineData("+5511987654321")]
-    [InlineData("(11) 98765-432")]
-    [InlineData("(1) 98765-4321")]
+    [InlineData("(11) 98765-4321")]
+    [InlineData("+55 11 98765-4321")]
+    [InlineData("+0511987654321")]
+    [InlineData("+5")]
     public void Given_InvalidValue_When_Constructed_Then_Throws(string value)
     {
         var act = () => new Phone(value);
@@ -19,9 +19,9 @@ public class PhoneTests
         act.Should().Throw<DomainException>();
     }
 
-    [Theory(DisplayName = "Valid Brazilian phone is accepted")]
-    [InlineData("(11) 98765-4321")]
-    [InlineData("(21) 3456-7890")]
+    [Theory(DisplayName = "Valid international phone is accepted")]
+    [InlineData("+5511987654321")]
+    [InlineData("5511987654321")]
     public void Given_ValidValue_When_Constructed_Then_KeepsValue(string value)
     {
         new Phone(value).Value.Should().Be(value);
@@ -30,6 +30,6 @@ public class PhoneTests
     [Fact(DisplayName = "Phones with the same value are structurally equal")]
     public void Given_SameValue_When_Compared_Then_Equal()
     {
-        new Phone("(11) 98765-4321").Should().Be(new Phone("(11) 98765-4321"));
+        new Phone("+5511987654321").Should().Be(new Phone("+5511987654321"));
     }
 }
